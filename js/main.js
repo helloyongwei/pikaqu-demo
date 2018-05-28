8f93912837705b246f98400f03bc884f4b2539ad
Helloyong <2628578405@qq.com>
2018-05-28 22:25:41 +0800

    add speed

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
 !function() {
+    let duration = 20
+    $('.actions').on('click', 'button', function(e) {
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed')
+        $button.addClass('active')
+            .siblings('.active').removeClass('active')
+        switch(speed) {
+            case 'slow':
+                duration = 100
+                break
+            case 'normal':
+                duration = 50
+                break
+            case 'fast':
+                duration = 10
+                break
+        }
+    })
     function writeCode(pre, code, fn) {
         let container = document.querySelector('#code')
         let styleTag = document.querySelector('#styleTag')
 
         let n = 0
-        let id = setInterval(()=>{
+        setTimeout (function run() {
             n+=1
             container.innerHTML = code.substring(0, n)
             styleTag.innerHTML = code.substring(0, n)
             container.scrollTop = container.scrollHeight
-            if (n >= code.length) {
-                window.clearInterval(id)
+            if (n < code.length) {
+                setTimeout(run, duration)
                 fn && fn.call()
             }
-        }, 10)
+        }, duration)
     }
 
     
@@ -142,4 +160,6 @@
 }
     `
     writeCode('', code)
+
+    
 }.call()
